5ca63755bebb0f946a6de7bac4f5c37040815d94
complex-freak <piuseverin06@hotmail.com>
2024-12-21 00:57:02 +0300

    Chapter One, Question One

---
+++ src/chapter_one/Question_One.java
@@ -0,0 +1,13 @@
+package chapter_one;
+
+public class Question_One
+{
+    public static void main (String[] args) {
+        System.out.println("**********************************");
+        System.out.println("*    Programming Assignment 1    *");
+        System.out.println("*     Computer Programming I     *");
+        System.out.println("*          Author: Zilla          ");
+        System.out.println("*   Date: Saturday, 21 December   ");
+        System.out.println("**********************************");
+    }
+}
