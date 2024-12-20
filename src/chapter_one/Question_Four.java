87f7cba5b44c1a12f93484a0bd49c742d3f48e33
complex-freak <piuseverin06@hotmail.com>
2024-12-21 01:20:35 +0300

    Chapter One, Question Four *Fix

--- src/chapter_one/Question_Four.java
+++ src/chapter_one/Question_Four.java
@@ -6,7 +6,7 @@ public class Question_Four {
         num1 = 75.35;
         num2 = -35.56;
         num3 = 15.76;
-        double average =  (double) (num1 + num2 + num3) /3;
+        double average =  (num1 + num2 + num3) /3;
         System.out.println("Num1 is " + num1 + ", num2 is " + num2 + ", num3 is " + num3 + " and average is " + average);
     }
 }
