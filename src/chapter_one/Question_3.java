d8e15def20c4c9a110fbd36ba7f2564b2a4a87d7
complex-freak <piuseverin06@hotmail.com>
2024-12-21 01:16:05 +0300

    Chapter One, Question Three

---
+++ src/chapter_one/Question_3.java
@@ -0,0 +1,12 @@
+package chapter_one;
+
+public class Question_3 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 125;
+        num2 = 28;
+        num3 = -25;
+        double average =  (double) (num1 + num2 + num3) /3;
+        System.out.println("Num1 is " + num1 + ", num2 is " + num2 + ", num3 is " + num3 + " and average is " + average);
+    }
+}
