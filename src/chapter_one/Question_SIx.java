6e78511fdd32622181d3ec2e420cec3c238e9d14
complex-freak <piuseverin06@hotmail.com>
2024-12-21 01:52:51 +0300

    Chapter One, Question Six

---
+++ src/chapter_one/Question_SIx.java
@@ -0,0 +1,15 @@
+package chapter_one;
+import java.util.Scanner;
+
+public class Question_SIx {
+    static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double number;
+        System.out.println("Enter any decimal number: ");
+        number = console.nextDouble();
+
+        System.out.println("The enter number to its nearest integer is " + Math.round(number));
+    }
+
+}
