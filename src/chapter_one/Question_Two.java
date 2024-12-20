ee9ea2ed46dcd4bccc556bc79cad740854473fa9
complex-freak <piuseverin06@hotmail.com>
2024-12-21 01:08:46 +0300

    Chapter One, Question Two

---
+++ src/chapter_one/Question_Two.java
@@ -0,0 +1,12 @@
+package chapter_one;
+
+public class Question_Two {
+    public static void main (String[] args) {
+        System.out.println("JJJJJJJJJJJJ     AAA       VV         VV       AAA       ");
+        System.out.println("     JJ         AA AA       VV       VV       AA AA      ");
+        System.out.println("     JJ        AA   AA       VV     VV       AA   AA     ");
+        System.out.println("     JJ       AAAAAAAAA       VV   VV       AAAAAAAAA    ");
+        System.out.println(" JJ  JJ      AA       AA       VV VV       AA       AA   ");
+        System.out.println(" JJJJJJ     AA         AA       VVV       AA         AA  ");
+    }
+}
