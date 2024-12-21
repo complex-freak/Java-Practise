8d131d65f927856ce77316331a2fed42cd6ca1f1
complex-freak <piuseverin06@hotmail.com>
2024-12-22 00:15:01 +0300

    Chapter One, Question Thirteen

---
+++ src/chapter_one/Question_Thirteen.java
@@ -0,0 +1,20 @@
+package chapter_one;
+import java.util.Scanner;
+
+public class Question_Thirteen {
+    static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int hours, minutes, seconds, totalSeconds;
+
+        System.out.print("Enter elapsed hours: ");
+        hours = console.nextInt();
+        System.out.print("Enter elapsed minutes: ");
+        minutes = console.nextInt();
+        System.out.print("Enter elapsed seconds: ");
+        seconds = console.nextInt();
+
+        totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+        System.out.println("Elapsed time: " + totalSeconds + " seconds.");
+    }
+}
