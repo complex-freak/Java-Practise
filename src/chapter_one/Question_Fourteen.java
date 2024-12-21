6b98276ce79c0f6a5cb560577d36b912961177e6
complex-freak <piuseverin06@hotmail.com>
2024-12-22 00:43:59 +0300

    Chapter One, Question Fourteen

---
+++ src/chapter_one/Question_Fourteen.java
@@ -0,0 +1,32 @@
+package chapter_one;
+import java.util.Scanner;
+
+public class Question_Fourteen {
+    static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double original_price_item, markedUp_percentage, sales_taxRate;
+        System.out.print("Enter original price of the item : ");
+        original_price_item = console.nextDouble();
+
+        System.out.print("Enter the marked-up percentage: ");
+        markedUp_percentage = console.nextDouble();
+
+        System.out.print("Enter the sales tax rate: ");
+        sales_taxRate = console.nextDouble();
+        System.out.println();
+
+        System.out.println("The original price of the item $" + original_price_item);
+        System.out.println("the marked-up percentage is " + markedUp_percentage + "%");
+        System.out.println("The sales tax-rate is " + sales_taxRate);
+
+        double sellingPrice, sales_tax, finalSellingPrice;
+        sellingPrice = original_price_item + (original_price_item * markedUp_percentage / 100);
+        sales_tax = sellingPrice * sales_taxRate / 100;
+        finalSellingPrice = sellingPrice + sales_tax;
+
+        System.out.println("The sales tax is " + sales_tax);
+        System.out.println("The store's selling price of an item is $" + sellingPrice);
+        System.out.println("The final selling price of an item is $" + finalSellingPrice);
+    }
+}
