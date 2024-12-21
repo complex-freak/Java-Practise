b6fcc6e0a2fb0f99a26eeeceb284221117c0a99b
complex-freak <piuseverin06@hotmail.com>
2024-12-22 02:01:13 +0300

    Chapter One, Question Nineteen

---
+++ src/chapter_one/Question_Nineteen.java
@@ -0,0 +1,28 @@
+package chapter_one;
+
+import java.util.Scanner;
+
+public class Question_Nineteen {
+    static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String str1, str2, str3;
+
+        System.out.print("Enter the first string: ");
+        str1 = console.nextLine();
+        System.out.print("Enter the second string: ");
+        str2 = console.nextLine();
+        System.out.print("Enter the third string: ");
+        str3 = console.nextLine();
+        System.out.println();
+
+        System.out.println("The permutation are :");
+        System.out.println(str1 + " " + str2 + " " + str3);
+        System.out.println(str1 + " " + str3 + " " + str2);
+        System.out.println(str2 + " " + str1 + " " + str3);
+        System.out.println(str2 + " " + str3 + " " + str2);
+        System.out.println(str3 + " " + str1 + " " + str2);
+        System.out.println(str3 + " " + str2 + " " + str1);
+
+    }
+}
