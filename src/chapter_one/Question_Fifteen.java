43c6c4eeee14bbaea7f97e9624a7d98436be0ce9
complex-freak <piuseverin06@hotmail.com>
2024-12-22 00:57:42 +0300

    Chapter One, Question Fifteen

---
+++ src/chapter_one/Question_Fifteen.java
@@ -0,0 +1,19 @@
+package chapter_one;
+import java.util.Scanner;
+
+public class Question_Fifteen {
+    static Scanner console = new Scanner(System.in);
+    static double PI = 3.141593;
+
+    public static void main(String[] args) {
+        double a, b, c, radius;
+        System.out.println("Enter the sides of the triangle a, b, and c: ");
+        a = console.nextDouble();
+        b = console.nextDouble();
+        c = console.nextDouble();
+
+        radius = (a + b + c) / 2 * PI;
+
+        System.out.println("The radius of the circle is " + radius);
+    }
+}
--- src/chapter_one/Question_Fourteen.java
+++ src/chapter_one/Question_Fourteen.java
@@ -18,14 +18,14 @@ public class Question_Fourteen {
 
         System.out.println("The original price of the item $" + original_price_item);
         System.out.println("the marked-up percentage is " + markedUp_percentage + "%");
-        System.out.println("The sales tax-rate is " + sales_taxRate);
+        System.out.println("The sales tax-rate is " + sales_taxRate + "%");
 
         double sellingPrice, sales_tax, finalSellingPrice;
         sellingPrice = original_price_item + (original_price_item * markedUp_percentage / 100);
         sales_tax = sellingPrice * sales_taxRate / 100;
         finalSellingPrice = sellingPrice + sales_tax;
 
-        System.out.println("The sales tax is " + sales_tax);
+        System.out.println("The sales tax is " + sales_tax + "%");
         System.out.println("The store's selling price of an item is $" + sellingPrice);
         System.out.println("The final selling price of an item is $" + finalSellingPrice);
     }
